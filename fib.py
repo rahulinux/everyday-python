80f5823c2209ace6cf071cfb6900ea51ca525471
rahulinux <loginrahul90@gmail.com>
2018-03-01 20:51:39 +0100

    improved algo

--- fib.py
+++ fib.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 
 def fib(n):
-    if n == 0:
+    # Time complexity : O(2n)
+    if n <= 1:
         return 0
-    elif n == 1:
-        return 1
     else:
         return fib(n-1) + fib(n-2) 
 
 
+def fib2(n):    
+    # O(1) 
+    a,b = 0,1
+    for _ in range(n):
+       a,b = b, a+b
+    return a
+
+def fib3(n):
+    # O(n)
+    f = [0,1]
+    for i in range(2,n):
+      f.append(f[i-1] + f[i-2])
+    return f
 
 
-for i in range(10):
-    print fib(i)
+
+for j in range(10):
+    print fib2(j)
+
+print fib3(10)
