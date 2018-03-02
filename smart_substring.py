a343270bc89a0f270f826042c4c4df6c91dd59de
rahulinux <loginrahul90@gmail.com>
2018-03-03 00:42:00 +0100

    added practice code

--- smart_substring.py
+++ smart_substring.py
@@ -17,21 +17,28 @@ Smarter approach (max 30 characters, no words are broken):
 
 
 def smart_substring(data):
-   i = 0 
-   result = []
-   for ch in data:
-      if i >= 26 and ch.strip() == '': 
-          break
-      result.append(ch)
-      i+=1
-   return ''.join(result)
+   result = ""
+   for word in data.split():
+      if (len(result) + len(word)) > 30: 
+          return result
+      result = (result + " " + word).strip()
+   return result
  
      
-
+def smart_substring_2(text):
+   number = 30
+   text_arr = text.split(" ")
+   final_string = ""
+   for text_val in text_arr:
+   	if (len(text_val) + len(final_string)) > number:
+   		return final_string
+   	final_string = (final_string + " " + text_val).strip()
+   return final_string
 
 print smart_substring("Featuring stylish rooms and moornings for recreation boats")
-print smart_substring("Welcome to the linux world we are going to start learning")
-print smart_substring("Breaking news, interchangeably termed late-breaking news and also known as a special report or special coverage or news bulletin, is a current issue that broadcasters feel warrants the interruption of")
+print smart_substring_2("Featuring stylish rooms and moornings for recreation boats")
+print smart_substring_2("Welcome to the linux world we are going to start learning")
+print smart_substring_2("Breaking news, interchangeably termed late-breaking news and also known as a special report or special coverage or news bulletin, is a current issue that broadcasters feel warrants the interruption of")
 
 
 
