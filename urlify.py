6e5bfdc451442c59ab471d35c9e14dd71aa31fd7
rahulinux <loginrahul90@gmail.com>
2018-03-06 09:55:13 +0100

    added practice code

---
+++ urlify.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+Write a method to replace all sapce in string with %20
+"""
+
+def urlify(url):
+   return url.replace(" ","%20")
+
+
+print urlify("Rahul Kisan Patil")
